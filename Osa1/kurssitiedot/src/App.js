609b0488452f1abde5617297b09ab144c45cf072
Nella Mantyniemi <nella.mantyniemi@tuni.fi>
2022-03-16 10:44:20 +0200

    Tehtävät 1 ja 2 ehkä??

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -1,25 +1,54 @@
+
+
+
+const Total = (props) => {  
+  return (
+    <div>
+      <p>{props.exercises}</p>   
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {  
+  return (
+    <div>
+      <Part part = {props.part} exercises = {props.exercises}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/> 
+      <Part part = {props.part3} exercises = {props.exercises3}/> 
+    </div>
+  )
+}
+
+const Header = (props) => {  
+  return (
+    <div>
+      <h1>{props.course}</h1>   
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const part = 'Fundamentals of React'
+  const exercises = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
   const part3 = 'State of a component'
+  const exercises2 = 7
   const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course ={course}/>
+      <Content part = {part} exercises = {exercises} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3}/>
+      <Total exercises = {exercises + exercises2 + exercises3}/>
     </div>
   )
 }
