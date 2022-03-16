d6b5a4ea5d96680375dd01ddaee6fd15b45f23b6
Nella Mantyniemi <nella.mantyniemi@tuni.fi>
2022-03-16 11:22:12 +0200

    1b tehtävät

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -36,19 +36,31 @@ const Header = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part = 'Fundamentals of React'
-  const exercises = 10
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  const exercises2 = 7
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course ={course}/>
-      <Content part = {part} exercises = {exercises} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3}/>
-      <Total exercises = {exercises + exercises2 + exercises3}/>
+      <Header course ={course.name}/>
+      <Content part = {course.parts[0].name} exercises = {course.parts[0].exercises} 
+        part2 = {course.parts[1].name} exercises2 = {course.parts[1].exercises} 
+        part3 = {course.parts[2].name} exercises3 = {course.parts[2].exercises}/>
+      <Total exercises = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </div>
   )
 }
